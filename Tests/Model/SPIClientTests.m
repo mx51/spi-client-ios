a7648f3655c20810b9df4f709c06572b94ae1dcf
Metin Avci <mavci@assemblypayments.com>
2019-08-29 16:41:50 +1000

    SP-247 Eftpos Address and PosId Validation

--- Tests/Model/SPIClientTests.m
+++ Tests/Model/SPIClientTests.m
@@ -18,7 +18,6 @@
 - (void)testSetPosId_OnInvalidLength_IsSet {
     // arrange
     static NSString *posId = @"12345678901234567";
-    static NSUInteger lengthOfPosId = 16;
     SPIClient *client = [[SPIClient alloc] init];
     
     // act
@@ -26,7 +25,154 @@
     
     // assert
     XCTAssertFalse([client.posId isEqualToString:posId]);
-    XCTAssertNotEqual(lengthOfPosId, posId.length);
+}
+
+- (void)testStart_OnInvalidLengthForPosId_IsSet {
+    // arrange
+    static NSString *posId = @"12345678901234567";
+    static NSString *eftposAddress = @"10.20.30.40";
+    static NSString *posVendorId = @"assembly";
+    static NSString *posVersion = @"2.6.3";
+    SPIClient *client = [[SPIClient alloc] init];
+    client.posId = posId;
+    client.eftposAddress = eftposAddress;
+    client.posVendorId = posVendorId;
+    client.posVersion = posVersion;
+    
+    // act
+    [client start];
+
+    
+    // assert
+    XCTAssertFalse([client.posId isEqualToString:posId]);
+}
+
+- (void)testSetPosId_OnValidCharacters_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos";
+    SPIClient *client = [[SPIClient alloc] init];
+    
+    // act
+    client.posId = posId;
+    
+    // assert
+    XCTAssertTrue([client.posId isEqualToString:posId]);
+}
+
+- (void)testStart_OnValidCharactersForPosId_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos";
+    static NSString *eftposAddress = @"10.20.30.40";
+    static NSString *posVendorId = @"assembly";
+    static NSString *posVersion = @"2.6.3";
+    SPIClient *client = [[SPIClient alloc] init];
+    client.posId = posId;
+    client.eftposAddress = eftposAddress;
+    client.posVendorId = posVendorId;
+    client.posVersion = posVersion;
+    
+    // act
+    [client start];
+    
+    // assert
+    XCTAssertTrue([client.posId isEqualToString:posId]);
+}
+
+- (void)testSetPosId_OnInvalidCharacters_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos@";
+    SPIClient *client = [[SPIClient alloc] init];
+    
+    // act
+    client.posId = posId;
+    
+    // assert
+    XCTAssertFalse([client.posId isEqualToString:posId]);
+}
+
+- (void)testStart_OnInvalidCharactersForPosId_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos@";
+    static NSString *eftposAddress = @"10.20.30.40";
+    static NSString *posVendorId = @"assembly";
+    static NSString *posVersion = @"2.6.3";
+    SPIClient *client = [[SPIClient alloc] init];
+    client.posId = posId;
+    client.eftposAddress = eftposAddress;
+    client.posVendorId = posVendorId;
+    client.posVersion = posVersion;
+    
+    // act
+    [client start];
+    
+    // assert
+    XCTAssertFalse([client.posId isEqualToString:posId]);
+}
+
+- (void)testSetEftposAddress_OnValidCharacters_IsSet {
+    // arrange
+    static NSString *eftposAddress = @"10.20.30.40";
+    SPIClient *client = [[SPIClient alloc] init];
+    
+    // act
+    client.eftposAddress = eftposAddress;
+    NSString *clientEftposAddress = [client.eftposAddress stringByReplacingOccurrencesOfString:@"ws://" withString:@""];
+    
+    // assert
+    XCTAssertTrue([clientEftposAddress isEqualToString:eftposAddress]);
+}
+
+- (void)testStart_OnValidCharactersForEftposAddress_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos";
+    static NSString *eftposAddress = @"10.20.30.40";
+    static NSString *posVendorId = @"assembly";
+    static NSString *posVersion = @"2.6.3";
+    SPIClient *client = [[SPIClient alloc] init];
+    client.posId = posId;
+    client.eftposAddress = eftposAddress;
+    client.posVendorId = posVendorId;
+    client.posVersion = posVersion;
+    
+    // act
+    [client start];
+    NSString *clientEftposAddress = [client.eftposAddress stringByReplacingOccurrencesOfString:@"ws://" withString:@""];
+    
+    // assert
+    XCTAssertTrue([clientEftposAddress isEqualToString:eftposAddress]);
+}
+
+- (void)testEftposAddress_OnInvalidCharacters_IsSet {
+    // arrange
+    static NSString *eftposAddress = @"10.20.30";
+    SPIClient *client = [[SPIClient alloc] init];
+    
+    // act
+    client.eftposAddress = eftposAddress;
+    NSString *clientEftposAddress = [client.eftposAddress stringByReplacingOccurrencesOfString:@"ws://" withString:@""];
+    
+    // assert
+    XCTAssertFalse([clientEftposAddress isEqualToString:eftposAddress]);
+}
+
+- (void)testStart_OnInvalidCharactersForEftposAddress_IsSet {
+    // arrange
+    static NSString *posId = @"RamenPos";
+    static NSString *eftposAddress = @"10.20.30";
+    static NSString *posVendorId = @"assembly";
+    static NSString *posVersion = @"2.6.3";
+    SPIClient *client = [[SPIClient alloc] init];
+    client.posId = posId;
+    client.eftposAddress = eftposAddress;
+    client.posVendorId = posVendorId;
+    client.posVersion = posVersion;
+    
+    // act
+    [client start];
+    NSString *clientEftposAddress = [client.eftposAddress stringByReplacingOccurrencesOfString:@"ws://" withString:@""];
+    
+    // assert
+    XCTAssertFalse([clientEftposAddress isEqualToString:eftposAddress]);
 }
 
 - (void)testUnpair_OnValid_IsSet {
